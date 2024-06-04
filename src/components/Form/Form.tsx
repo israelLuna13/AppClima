f9adb5820d10f5ee61d96afdff0176320ec67c96
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-06-04 13:47:44 -0600

    se agrego el state del form , del error y validaciones del form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,42 @@
+import { useState } from 'react'
 import { countries } from '../../data/countries'
 import styles from './Form.module.css'
+import { SearchType } from '../../types'
+import Alert from '../Alert/Alert'
 
 
 export default function Form() {
+    const [search,setSearch] = useState<SearchType>({
+        city:'',
+        country:''
+    })
+
+    const[alert,setAlert] = useState('')
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>| React.ChangeEvent<HTMLSelectElement> ) =>{
+        setSearch({
+            ... search, [e.target.name]:e.target.value
+        })
+
+
+    }
+    const handleSubmit = ( e: React.FormEvent<HTMLFormElement>) => {
+
+        e.preventDefault()
+
+        if(Object.values(search).includes('')){
+            setAlert('Todos los campos son obligatorios')
+            return
+
+        }
+    }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form}
+    onSubmit={handleSubmit}
+    >
+        {alert && <Alert>{alert}</Alert>}
 
         <div className={styles.field}>
             <label htmlFor="city">Ciudad:</label>
@@ -13,13 +45,19 @@ export default function Form() {
                 type="text"
                 name='city'
                 placeholder='Ciudad'
+                value={search.city}
+                onChange={handleChange}
             />
         </div>
 
         <div className={styles.field}>
-            <label htmlFor="city">Pais:</label>
-                
-                <select>
+            <label htmlFor="country">Pais:</label>
+                <select
+                id='country'
+                value={search.country}
+                name='country'
+                onChange={handleChange}
+                >
                 
             <option value="">---Seleccione un pais---</option>
             {countries.map(country => (
