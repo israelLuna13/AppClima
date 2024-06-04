59baceb5f586b1cd82af28bf238232d857e33419
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-06-04 13:14:51 -0600

    Se agrego el formulario y sus estilos css

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,40 @@
+import { countries } from '../../data/countries'
+import styles from './Form.module.css'
+
+
+export default function Form() {
+  return (
+    <form className={styles.form}>
+
+        <div className={styles.field}>
+            <label htmlFor="city">Ciudad:</label>
+            <input 
+                id='city'
+                type="text"
+                name='city'
+                placeholder='Ciudad'
+            />
+        </div>
+
+        <div className={styles.field}>
+            <label htmlFor="city">Pais:</label>
+                
+                <select>
+                
+            <option value="">---Seleccione un pais---</option>
+            {countries.map(country => (
+                    <option 
+                    value={country.code}
+                    key={country.code}
+                    >
+                        {country.name}
+                    </option>
+            ))}
+
+                </select>
+        </div>
+
+        <input className={styles.submit} type="submit" value='Consultar clima'/>
+    </form>
+  )
+}
