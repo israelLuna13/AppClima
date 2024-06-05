4c953442d89bfcbeb2f8acfc61e8e244d4a7614c
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-06-04 20:23:07 -0600

    tipado de la respuesta de la api con valibot

--- src/hoooks/useWeather.ts
+++ src/hoooks/useWeather.ts
@@ -1,6 +1,7 @@
 import { SearchType } from "../types"
 import axios from 'axios'
-import {z} from 'zod'
+import {object,string,number,InferOutput,parse} from 'valibot'
+//import {z} from 'zod'
 export default function useWeather() {
 
 
@@ -20,19 +21,32 @@ export default function useWeather() {
   // }
 
 
-  // zod
-  //esquima
-  const Weather = z.object({
-    name:z.string(),
-    main:z.object({
-      temp:z.number(),
-      temp_max:z.number(),
-      temp_min:z.number(),
+  // // zod
+  // //esquima
+  // const Weather = z.object({
+  //   name:z.string(),
+  //   main:z.object({
+  //     temp:z.number(),
+  //     temp_max:z.number(),
+  //     temp_min:z.number(),
       
-    })
-  })
+  //   })
+  // })
 
-  type Weather = z.infer<typeof Weather>
+  // type Weather = z.infer<typeof Weather>
+
+  //esquema esperado
+  const WeatherShema = object({
+       name:string(),
+       main:object({
+          temp:number(),
+          temp_max:number(),
+          temp_min:number(),
+  })
+})
+
+type Weather = InferOutput<typeof WeatherShema>
+ 
 
 
     const fetchWeather=async(search:SearchType) =>{
@@ -53,20 +67,27 @@ export default function useWeather() {
               const {data:weatherResult} = await axios(weatherUrl)
 
               //validamos si la respuesta obtenido tiene la estructura de nuestro esquima
-              const result = Weather.safeParse(weatherResult)
-              if(result.success)
-                {
-                  console.log(result.data.name)
-                  console.log(result.data.main.temp)
+              // const result = Weather.safeParse(weatherResult)
+              // if(result.success)
+              //   {
+              //     console.log(result.data.name)
+              //     console.log(result.data.main.temp)
 
 
-                }                  // //castear el type 
+              //   }                  // //castear el type 
             // //type guards 
             // const result = isWeatherResponse(weatherResult)
 
             // if(result){
             //   console.log(weatherResult.name)
             // }
+
+            //valibot
+            //validamos si los esquemas tienen la misma estructura
+            const result = parse(WeatherShema,weatherResult)
+            if(result){
+              console.log(result.name)
+            }
 
         } catch (error) {
             console.log(error)
