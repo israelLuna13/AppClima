e74909f519db70c8a61067ec7347d86e214b6617
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-06-06 14:28:17 -0600

    Respuesta de la api en el state, mostramos los resultados y se agrego un spinner de carga

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import styles from "./App.module.css"
-import Form from "./components/Form/Form"
-import useWeather from "./hoooks/useWeather"
+import styles from "./App.module.css";
+import Form from "./components/Form/Form";
+import Spinner from "./components/Spinner/Spinner";
+import WeatherDetail from "./components/WeatherDetail/WeatherDetail";
+import useWeather from "./hoooks/useWeather";
 function App() {
-
-  const {fetchWeather} = useWeather()
+  const { fetchWeather, weather, hasWeatherData, loading } = useWeather();
   return (
     <>
-     <h1 className={styles.title}>Buscador de Clima</h1>
-     <div className={styles.container}>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+      <div className={styles.container}>
+        <Form fetchWeather={fetchWeather} />
 
-      <Form
-          fetchWeather = {fetchWeather}
-      />
-      
-        <p>2</p>
-     </div>
-
+        {loading && (
+          <p>
+            <Spinner />.
+          </p>
+        )}
+        {hasWeatherData && <WeatherDetail weather={weather} />}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/hoooks/useWeather.ts
+++ src/hoooks/useWeather.ts
@@ -1,18 +1,41 @@
-import { SearchType } from "../types"
-import axios from 'axios'
-import {object,string,number,InferOutput,parse} from 'valibot'
-//import {z} from 'zod'
+import { SearchType } from "../types";
+import axios from "axios";
+import { useMemo, useState } from "react";
+//import {object,string,number,InferOutput,parse} from 'valibot'
+import { z } from "zod";
+// // zod
+// //esquima
+const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number(),
+  }),
+});
+
+export type Weather = z.infer<typeof Weather>;
+
 export default function useWeather() {
+  const [weather, setWeather] = useState<Weather>({
+    name: "",
+    main: {
+      temp: 0,
+      temp_min: 0,
+      temp_max: 0,
+    },
+  });
 
+  const [loading, setLoading] = useState(false);
 
-  //type guard o assertion 
+  //type guard o assertion
   //validamos que la respuesta que obtengamos de la apli tenga esta estructura
   //esta funcion recibira un dato desconocio por el momento
 
   // function isWeatherResponse(weather:unknown):weather is Weather{
   //   return(
-  //     Boolean(weather) && 
-  //     typeof weather === 'object' && 
+  //     Boolean(weather) &&
+  //     typeof weather === 'object' &&
   //     typeof (weather as Weather).name === 'string' &&
   //     typeof (weather as Weather).main.temp === 'number' &&
   //     typeof (weather as Weather).main.temp_max === 'number' &&
@@ -20,82 +43,65 @@ export default function useWeather() {
   //   )
   // }
 
-
-  // // zod
-  // //esquima
-  // const Weather = z.object({
-  //   name:z.string(),
-  //   main:z.object({
-  //     temp:z.number(),
-  //     temp_max:z.number(),
-  //     temp_min:z.number(),
-      
+  //esquema esperado
+  //   const WeatherShema = object({
+  //        name:string(),
+  //        main:object({
+  //           temp:number(),
+  //           temp_max:number(),
+  //           temp_min:number(),
   //   })
   // })
 
-  // type Weather = z.infer<typeof Weather>
+  //type Weather = InferOutput<typeof WeatherShema>
 
-  //esquema esperado
-  const WeatherShema = object({
-       name:string(),
-       main:object({
-          temp:number(),
-          temp_max:number(),
-          temp_min:number(),
-  })
-})
+  const fetchWeather = async (search: SearchType) => {
+    //api key
+    const appId = import.meta.env.VITE_API_KEY;
 
-type Weather = InferOutput<typeof WeatherShema>
- 
+    setLoading(true);
+    try {
+      //hacemos el primer llamado a la api
+      const geoUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.country}&appid=${appId}`;
+      const { data } = await axios(geoUrl);
+      // Desestructuramos el objeto data para obtener las coordenadas
+      const {
+        coord: { lat, lon },
+      } = data;
 
+      //segundo llamado a la api
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}&units=imperial`;
 
-    const fetchWeather=async(search:SearchType) =>{
-        try {
-          //api key
-            const appId =import.meta.env.VITE_API_KEY
+      //zood
+      const { data: weatherResult } = await axios(weatherUrl);
 
-            //hacemos el primer llamado a la api
-            const geoUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.country}&appid=${appId}`
-            const {data} = await axios(geoUrl)
-            // Desestructuramos el objeto data para obtener las coordenadas
-            const { coord: { lat, lon } } = data;
+      //validamos si la respuesta obtenido tiene la estructura de nuestro esquima
+      const result = Weather.safeParse(weatherResult);
+      if (result.success) {
+        setWeather(result.data);
+      }
 
-            //segundo llamado a la api
-            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}&units=imperial`
-            
-              //zood
-              const {data:weatherResult} = await axios(weatherUrl)
+      //castear el type
+      // //type guards
+      // const result = isWeatherResponse(weatherResult)
 
-              //validamos si la respuesta obtenido tiene la estructura de nuestro esquima
-              // const result = Weather.safeParse(weatherResult)
-              // if(result.success)
-              //   {
-              //     console.log(result.data.name)
-              //     console.log(result.data.main.temp)
+      // if(result){
+      //   console.log(weatherResult.name)
+      // }
 
-
-              //   }                  // //castear el type 
-            // //type guards 
-            // const result = isWeatherResponse(weatherResult)
-
-            // if(result){
-            //   console.log(weatherResult.name)
-            // }
-
-            //valibot
-            //validamos si los esquemas tienen la misma estructura
-            const result = parse(WeatherShema,weatherResult)
-            if(result){
-              console.log(result.name)
-            }
-
-        } catch (error) {
-            console.log(error)
-
-        }
+      //valibot
+      //validamos si los esquemas tienen la misma estructura
+      // const result = parse(WeatherShema,weatherResult)
+      // if(result){
+      //   console.log(result.name)
+      // }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-  return (
+  };
 
-    {fetchWeather}
-  )
+  const hasWeatherData = useMemo(() => weather.name, [weather]);
+  return { fetchWeather, weather, hasWeatherData, loading };
 }
